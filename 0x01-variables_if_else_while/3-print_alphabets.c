6918501b95bd16aeb7c3262252e5f83732d2c48b
Topazytee <peakybeetle@gmail.com>
2023-10-19 11:24:57 -0700

    3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char v;
+
+	char m;
+
+	v = 'a';
+	m = 'A';
+	while
+		(v <= 'z') {
+			putchar(v);
+			v++;
+		}
+	while
+		(m <= 'Z') {
+			putchar (m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
