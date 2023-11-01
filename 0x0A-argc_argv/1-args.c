49a938ae1e38f8a741649d0ee836b4808e3b405c
Topazytee <peakybeetle@gmail.com>
2023-11-01 00:12:10 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * Return: 0
+ */
+
+int main(int argc)
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
