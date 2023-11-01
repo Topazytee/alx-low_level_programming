8e684a77149d6b94a9a37a880288b33e06d30222
Topazytee <peakybeetle@gmail.com>
2023-10-31 21:42:39 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,4 +10,21 @@
 
 char *_strdup(char *str)
 {
+	char *aaa;
+	int m, n = 0;
 
+	if (str == NULL)
+		return (NULL);
+	m = 0;
+	while (str[m] != '\0')
+		m++;
+
+	aaa = malloc(sizeof(char) * (m + 1));
+
+	if (aaa == NULL)
+		return (NULL);
+
+	for (n = 0; str[n]; n++)
+		aaa[n] = str[n];
+	return (aaa);
+}
