d6501fb97acde1ecd90137cbb8f4b2b7901a5eb7
Topazytee <peakybeetle@gmail.com>
2023-11-07 09:14:11 -0800

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+
+/**
+ * File: function_like_macro
+ * @x: input value (int)
+ * Desc: header file that computes the absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
